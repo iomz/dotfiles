0f35b2f78f4800d2e59ad8469fa3f744000754f5
Iori Mizutani <iori.mizutani@gmail.com>
2023-05-24 21:54:03 +0200

    fix: use /etc/rpi-issue to detect RPi

--- dot_config/zsh/rc.d/03-polaris.zsh
+++ dot_config/zsh/rc.d/03-polaris.zsh
@@ -4,6 +4,18 @@
 # Download and link binaries
 #
 
+# For RPi, don't do much
+if [[ -e /etc/rpi-issue ]]; then
+    zinit from'gh-r' lbin'!' light-mode no'compile' for \
+        compile'key-bindings.zsh' \
+        dl="$(builtin print -c -- https://raw.githubusercontent.com/junegunn/fzf/master/{shell/{'key-bindings.zsh;','completion.zsh -> _fzf;'},'bin/fzf-tmux -> fzf-tmux;',man/{'man1/fzf.1 -> $ZPFX/share/man/man1/fzf.1;','man1/fzf-tmux.1 -> $ZPFX/share/man/man1/fzf-tmux.1;'}})" \
+        lbin'!fzf;!fzf-tmux;' \
+        src'key-bindings.zsh' \
+        @junegunn/fzf \
+        @x-motemen/ghq
+    return
+fi
+
 # GitHub {{{
 zinit from'gh-r' lbin'!' light-mode no'compile' for \
     @sharkdp/bat \
