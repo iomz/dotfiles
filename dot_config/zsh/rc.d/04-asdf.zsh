9fdec2fca6ddbbac39bf73aff7536056205bea8b
Iori Mizutani <iori.mizutani@gmail.com>
2023-05-28 22:15:33 +0200

    refactor: use asdf instead of nvm, pyenv, rubyenv

--- dot_config/zsh/rc.d/04-asdf.zsh
+++ dot_config/zsh/rc.d/04-asdf.zsh
@@ -14,11 +14,28 @@ fi
 fpath=(${ASDF_DIR}/completions $fpath)
 
 # install dependencies for plugins
-# macOS
-#brew install gpg gawk
-#brew install openssl readline sqlite3 xz zlib tcl-tk
-# Debian
-#apt install dirmngr gpg curl gawk
-#apt install build-essential libssl-dev zlib1g-dev \
-# libbz2-dev libreadline-dev libsqlite3-dev curl \
-# libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+case ${OSTYPE} in
+    darwin*)
+        # golang
+        export GOPATH=$HOME/go
+        path=( $GOPATH/bin(N-/) $HOME/.ghg/bin(N-/) $path )
+        # nodejs
+        #brew install gpg gawk
+        # python
+        #brew install openssl readline sqlite3 xz zlib tcl-tk
+        # ruby
+        #brew install openssl@3 readline libyaml gmp
+        export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@3)"
+        ;;
+    linux*)
+        # golang
+        export GOPATH=$HOME/go
+        path=( $GOPATH/bin(N-/) $HOME/.ghg/bin(N-/) $path )
+        # nodejs
+        #apt install dirmngr gpg curl gawk
+        # python
+        #apt install build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+        # ruby
+        # apt-get install autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+        ;;
+esac
