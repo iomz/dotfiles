8ca5c9240095a23d427a6e5d19edf012c1c5cdda
Iori Mizutani <iori.mizutani@gmail.com>
2023-05-29 13:01:42 +0200

    feat: add duf

--- dot_config/zsh/rc.d/03-polaris.zsh
+++ dot_config/zsh/rc.d/03-polaris.zsh
@@ -14,25 +14,26 @@ if [[ -e /etc/rpi-issue ]]; then
         @junegunn/fzf \
         @x-motemen/ghq
 else
-# GitHub {{{
-zinit from'gh-r' lbin'!' light-mode no'compile' for \
-    @sharkdp/bat \
-    lbin'!bat-modules;!batdiff;!batgrep;!batman;!batpipe;!batwatch;!prettybat;' \
-    @eth-p/bat-extras \
-    @dandavison/delta \
-    cp'**/exa.zsh->_exa' \
-    @ogham/exa \
-    compile'key-bindings.zsh' \
-    dl="$(builtin print -c -- https://raw.githubusercontent.com/junegunn/fzf/master/{shell/{'key-bindings.zsh;','completion.zsh -> _fzf;'},'bin/fzf-tmux -> fzf-tmux;',man/{'man1/fzf.1 -> $ZPFX/share/man/man1/fzf.1;','man1/fzf-tmux.1 -> $ZPFX/share/man/man1/fzf-tmux.1;'}})" \
-    lbin'!fzf;!fzf-tmux;' \
-    src'key-bindings.zsh' \
-    @junegunn/fzf \
-    @Songmu/ghg \
-    @x-motemen/ghq \
-    @sharkdp/hyperfine \
-    lbin'!nvim' \
-    ver'nightly' \
-    @neovim/neovim \
-    @r-darwish/topgrade
-# }}}
+    # GitHub {{{
+    zinit from'gh-r' lbin'!' light-mode no'compile' for \
+        @sharkdp/bat \
+        lbin'!bat-modules;!batdiff;!batgrep;!batman;!batpipe;!batwatch;!prettybat;' \
+        @eth-p/bat-extras \
+        @dandavison/delta \
+        @muesli/duf \
+        cp'**/exa.zsh->_exa' \
+        @ogham/exa \
+        compile'key-bindings.zsh' \
+        dl="$(builtin print -c -- https://raw.githubusercontent.com/junegunn/fzf/master/{shell/{'key-bindings.zsh;','completion.zsh -> _fzf;'},'bin/fzf-tmux -> fzf-tmux;',man/{'man1/fzf.1 -> $ZPFX/share/man/man1/fzf.1;','man1/fzf-tmux.1 -> $ZPFX/share/man/man1/fzf-tmux.1;'}})" \
+        lbin'!fzf;!fzf-tmux;' \
+        src'key-bindings.zsh' \
+        @junegunn/fzf \
+        @Songmu/ghg \
+        @x-motemen/ghq \
+        @sharkdp/hyperfine \
+        lbin'!nvim' \
+        ver'nightly' \
+        @neovim/neovim \
+        @r-darwish/topgrade
+    # }}}
 fi
